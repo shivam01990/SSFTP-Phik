6d7c706694ef4ded740e23a0dd282486be4ecfaa
Shivam Srivastava <shivam01990@gmail.com>
2015-12-17 20:01:54 +0530

    Version

---
+++ SFTPGetConsole Version 2/SFTPGetConsole/DBProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFTPGetConsole
+{
+    public class DBProvider
+    {
+        #region--Instance--
+        public static DBProvider Instance = new DBProvider();
+        #endregion
+
+        #region--Download Parameters--
+        public List<SFTPmap> DownloadParameters()
+        {
+            List<SFTPmap> rType = new List<SFTPmap>();
+            using (IBSEntities db = new IBSEntities())
+            {
+                rType = db.SFTPmaps.ToList();
+            }
+            return rType;
+        }
+        #endregion
+    }
+}
